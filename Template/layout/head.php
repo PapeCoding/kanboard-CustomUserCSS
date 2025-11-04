524d6a07676ca834ecc5a38a3f8ec337b38743e9
Sebi <Sebi@PapeCoding.de>
2025-11-04 12:09:06 +0100

    Fixing output and UserID fetching

--- Template/layout/head.php
+++ Template/layout/head.php
@@ -1,5 +1,6 @@
 <style>
-	$css_file = DATA_DIR . '/custom_css/user_' . $this->userSession->getId() . '.css';
-	$css = file_exists($css_file) ? file_get_contents($css_file) : '';
-
+	<?php
+	$css_file = DATA_DIR . '/custom_css/user_' . $this->user->getid() . '.css';
+	print(file_exists($css_file) ? file_get_contents($css_file) : '');
+	?>
 </style>
